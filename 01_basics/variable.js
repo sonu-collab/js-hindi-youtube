67dd61527261a1120efd27519efe41fd96689d14
sonu-collab <121295279+sonu-collab@users.noreply.github.com>
2024-04-10 12:18:13 +0000

    all about variable

---
+++ 01_basics/variable.js
@@ -0,0 +1,9 @@
+const accountId = 12345
+let  accountEmail = "sonu@gmail.com"
+var accountPassword = "12345"
+accountCity = "Noida"
+let accountState;
+//accountId = 56789
+console.table([accountEmail,accountId,accountPassword,accountCity,accountState])
+
+
