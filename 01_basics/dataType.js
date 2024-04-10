d3249bd14e7011eced2bd3e055253f29a85ab280
sonu-collab <121295279+sonu-collab@users.noreply.github.com>
2024-04-10 13:22:42 +0000

    about datatype

---
+++ 01_basics/dataType.js
@@ -0,0 +1,19 @@
+// console.log(3 + 3 )
+// console.log("Hello, World")
+
+let name = "Hello"
+let age = 12
+let isLoggedin = false
+
+// number
+// bigint
+// string = ""
+// boolean = True, False
+// null = standalone value or Datatype
+// undefined
+
+console.log(typeof isLoggedin);
+
+
+
+
