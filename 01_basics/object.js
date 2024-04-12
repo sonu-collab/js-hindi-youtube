01fd8b1a8a1458b7bf4ecff03ee549260d1423b2
sonu-collab <121295279+sonu-collab@users.noreply.github.com>
2024-04-12 06:18:45 +0000

    objectinjs

---
+++ 01_basics/object.js
@@ -0,0 +1,10 @@
+const jsUser = {
+    name : "Sonu",
+    age : 18,
+    location : "Noida",
+    email : "Sonu@google.com",
+    isLoggedIn : false
+}
+console.log(jsUser.email)
+console.log(jsUser["email"])
+
