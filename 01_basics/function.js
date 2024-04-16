fd3d11521a1a5dc085ec150a191ce192da91e6ab
sonu-collab <121295279+sonu-collab@users.noreply.github.com>
2024-04-16 13:14:47 +0000

    function in js

---
+++ 01_basics/function.js
@@ -0,0 +1,16 @@
+function addTwoNumber(number1, number2){
+    // console.log(number1 + number2)
+}
+
+addTwoNumber(1, 2)
+
+function calculateCartPrice(...num1) {
+    return num1
+}
+// console.log(calculateCartPrice(200, 400, 800))
+
+function chai(){
+    console.log(this);
+}
+
+chai()
