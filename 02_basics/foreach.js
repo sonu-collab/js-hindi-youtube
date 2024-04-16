6953073b104f96564e6998adc4d2d0a09b869876
sonu-collab <121295279+sonu-collab@users.noreply.github.com>
2024-04-16 17:37:35 +0000

    forEach loop

---
+++ 02_basics/foreach.js
@@ -0,0 +1,11 @@
+const value = [1, 2, 3, 4, 5]
+value.forEach(function (val){
+    // console.log(val);
+})
+
+const coding = ["java", "python", "javascript", "ruby"]
+coding.forEach( (item) => {
+    console.log(item)
+})
+
+
