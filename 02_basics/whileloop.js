e5320a53e3e687b07524424850c6968b2e3a8fbc
sonu-collab <121295279+sonu-collab@users.noreply.github.com>
2024-04-16 15:55:16 +0000

    whileloop in js

---
+++ 02_basics/whileloop.js
@@ -0,0 +1,13 @@
+let index = 0
+while (index <= 10) {
+    // console.log(`value of index is ${index}`);
+     index = index + 1
+}
+
+// do-while 
+
+let score = 1
+do {
+    console.log(`score is ${score}`);
+    score++
+} while (score <= 10);
