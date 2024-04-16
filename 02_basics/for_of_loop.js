8467af51d3f098a7053fe2c4794553b2ea5545e0
sonu-collab <121295279+sonu-collab@users.noreply.github.com>
2024-04-16 17:14:24 +0000

    for of loop

---
+++ 02_basics/for_of_loop.js
@@ -0,0 +1,11 @@
+let arr = [1, 2, 3, 4, 5]
+for (const num of arr) {
+    // console.log(num);
+}
+
+const greeting = "Hello, World"
+
+for (const greet of greeting) {
+    console.log(greet)
+    
+}
